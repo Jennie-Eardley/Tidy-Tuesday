6a59ca431178e76253b525ef6b51a7764f213680
Jennie Eardley <jennie.eardley@outlook.com>
2020-09-24 21:45:08 +0100

    adds plot

--- himalayas/climbing.R
+++ himalayas/climbing.R
@@ -9,4 +9,9 @@ expeditions <- tuesdata$expeditions
 expeditions <- expeditions %>% 
   mutate(expedition_length = highpoint_date - basecamp_date)
 
+expeditions %>% 
+  filter(!is.na(expedition_length)) %>% 
+  ggplot() + 
+  geom_point(aes(x = year, y = expedition_length, col = oxygen_used)) +
+  facet_wrap(~season)
 
