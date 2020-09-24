6acb6bcdca8a97e4677a09834af5f16b6e25da1a
Jennie Eardley <jennie.eardley@outlook.com>
2020-09-24 20:37:06 +0100

    adds data

---
+++ himalayas/climbing.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(tidytuesdayR)
+
+tuesdata <- tidytuesdayR::tt_load('2020-09-22')
+peaks <- tuesdata$peaks
+members <- tuesdata$members
+expeditions <- tuesdata$expeditions
+
+
+
+
