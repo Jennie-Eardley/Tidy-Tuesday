543b9333992914bea418ea36e54683f39e977254
Jennie Eardley <jennie.eardley@outlook.com>
2020-09-25 15:52:58 +0100

    reorders factors

--- himalayas/climbing.R
+++ himalayas/climbing.R
@@ -9,9 +9,11 @@ expeditions <- tuesdata$expeditions
 expeditions <- expeditions %>% 
   mutate(expedition_length = highpoint_date - basecamp_date)
 
+expeditions$season = factor(expeditions$season, levels = c("Spring", "Summer", "Autumn", "Winter"))
+
 expeditions %>% 
   filter(!is.na(expedition_length)) %>% 
   ggplot() + 
-  geom_point(aes(x = year, y = expedition_length, col = oxygen_used)) +
-  facet_wrap(~season)
+  geom_point(aes(x = year, y = expedition_length, col = season)) +
+  facet_wrap(~oxygen_used)
 
