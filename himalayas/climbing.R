56bd6f866dcc5de16288e07b7743a358f0af8a42
Jennie Eardley <jennie.eardley@outlook.com>
2020-09-25 16:26:04 +0100

    adds labels

--- himalayas/climbing.R
+++ himalayas/climbing.R
@@ -6,14 +6,27 @@ peaks <- tuesdata$peaks
 members <- tuesdata$members
 expeditions <- tuesdata$expeditions
 
+# calculating expedition length
+
 expeditions <- expeditions %>% 
   mutate(expedition_length = highpoint_date - basecamp_date)
 
+# reordering factors
 expeditions$season = factor(expeditions$season, levels = c("Spring", "Summer", "Autumn", "Winter"))
+
+expeditions <- expeditions %>% 
+  filter(!is.na(oxygen_used)) %>% 
+  mutate(oxygen_usage = ifelse(str_detect(oxygen_used, "TRUE"), "Oxygen Used", "Oxygen Not Used"))
 
 expeditions %>% 
   filter(!is.na(expedition_length)) %>% 
   ggplot() + 
-  geom_point(aes(x = year, y = expedition_length, col = season)) +
-  facet_wrap(~oxygen_used)
+  geom_point(aes(x = year, y = expedition_length, col = oxygen_usage)) 
+ facet_wrap(~season) +
+  labs(x = "Year",
+       y = "Length of Expedition (days)",
+       title = "Use of Oxygen in Himilaya Expeditions",
+       caption = "Data Source: The Himalayan Database and Alex Cookson") +
+  theme(legend.title = element_blank()) 
+  
 
